030ae0d937a040375cea2d5c7511a9dad481aa41
ccortes <cicc81@gmail.com>
2023-09-30 01:02:44 -0600

    Commit con ultimos temas previo a creacion de proyecto con html,css y js

---
+++ arraysObjt.js
@@ -0,0 +1,27 @@
+var articulos = [
+    {nombre: "Bici", costo:3000},
+    {nombre: "TV", costo:2500},
+    {nombre: "Libro", costo:320},
+    {nombre: "Celular", costo:10000},
+    {nombre: "LapTop", costo:20000},
+    {nombre: "Teclado", costo:500},
+    {nombre: "Audifonos", costo:1700},
+];
+
+var articulosFiltrados = articulos.filter(function(articulo){
+    return articulo.costo <= 500
+});
+
+var articulosMapeados = articulos.map(function(articulo){
+    return articulo.nombre
+});
+
+var articulosEncontrados = articulos.find(function(articulo){
+    return articulo.nombre === "LapTop"
+});
+
+articulos.forEach(function (articulo) {
+    console.log(articulo.nombre);
+});
+
+var articulosBaratos 
---
+++ funcConstru.js
@@ -0,0 +1,11 @@
+function auto(marca, modelo, annio) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.annio = annio;
+}
+
+var autos = [];
+
+
+var marca = prompt("Ingresa la marca del auto ");
+var marca = prompt("Ingresa la marca del auto ");
